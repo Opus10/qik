06a275b2851638dc2dcd434bce33094913a62170
Wesley Kendall <wesleykendall@gmail.com>
2024-09-08 09:45:43 -0500

    Fix module filtering

--- qik/runner.py
+++ qik/runner.py
@@ -119,15 +119,17 @@ class DAGPool:
 class Graph:
     """A graph of runnables."""
 
-    def __init__(self, cmds: Iterable[str]):
+    def __init__(self, cmds: Iterable[str], modules: Iterable[str]):
         self.cmds = cmds
         self._view = None
+        modules = frozenset(modules)
 
         # Construct the graph
         self._nodes: dict[str, Runnable] = {
             name: runnable
             for cmd in cmds
             for name, runnable in qik.cmd.load(cmd).runnables.items()
+            if not modules or not runnable.module or runnable.module in modules
         }
         self._graph = {
             "up": collections.defaultdict(set[str]),
@@ -324,7 +326,7 @@ def _get_graph() -> Graph:
         cmds = list(qik.cmd.ls())
 
     try:
-        graph = Graph(cmds)
+        graph = Graph(cmds, modules=modules)
     except RecursionError as exc:
         raise qik.errors.GraphCycle("Cycle detected in DAG.") from exc
 
@@ -336,9 +338,6 @@ def _get_graph() -> Graph:
 
     if qik_ctx.since:
         graph = graph.filter_since(qik_ctx.since)
-
-    if qik_ctx.modules:
-        graph = graph.filter_modules(modules)
 
     return graph
 
